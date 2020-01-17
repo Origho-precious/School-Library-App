2580f73eec4302618224502ac4e0cd9d368ed27d
Origho-precious <53418429+Origho-precious@users.noreply.github.com>
2020-01-17 13:05:24 +0100

    Added localStorage

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Global Variables 
 const showLibraryForm = document.getElementById('showbook-btn');
 const bookForm = document.querySelector('.main');
 const bookName = document.getElementById('book-name');
@@ -9,16 +11,42 @@ const tHead = document.querySelector('.table-head');
 const tBody = document.querySelector('.table-body');
 
 
+
+// Display BookForm
 showLibraryForm.addEventListener('click', () => {
     bookForm.style.display = 'block';
 });
 
+// Clear inputFields function
 const clearFields = () => {
     bookName.value = '';
     bookAuthor.value = '';
     bookCategory.value = '';
+};
+
+
+// LocalStorage Function
+const getBookFromlocalStorage = () => {
+    const warning = document.querySelector('.ls-warning');
+    if(localStorage.length == 0){
+        libraryTitle.style.display = 'none';
+        tHead.style.visibility = 'hidden';
+        // warning.innerHTML = 'No Book Yet, Click on <span>Add Book To Library</span>';
+    }else{
+        libraryTitle.style.display = 'block';
+        tHead.style.visibility = 'visible';
+        warning.style.display = 'none';
+        tBody.innerHTML = localStorage.getItem('tbody')
+    }
+};
+
+const storeBookInLocalStorage = (tBody) => {
+    localStorage.setItem('tbody', tBody);
 }
 
+window.addEventListener('load', getBookFromlocalStorage);
+
+// Add Book
 addBookBtn.addEventListener('click', () => {
     if (bookName.value === '' || bookAuthor.value === '' || bookCategory.value === '') {
         const errorMsg = document.querySelector('#error');
@@ -34,18 +62,24 @@ addBookBtn.addEventListener('click', () => {
         bookForm.style.display = 'none';
         libraryTitle.style.display = 'block';
         
-    tHead.style.visibility = 'visible';
+        tHead.style.visibility = 'visible';
         const row = document.createElement('tr');
+
         row.innerHTML = `
                             <tr>
                                 <td>${bookName.value}</td>
                                 <td>${bookAuthor.value}</td>
                                 <td>${bookCategory.value}</td>
+                                <td class="delete-btn">X</td>
                             </tr>
                         `
-
+            
         tBody.append(row);
+
+        storeBookInLocalStorage(tBody.innerHTML);
 
         clearFields();
     }
 });
+
+
